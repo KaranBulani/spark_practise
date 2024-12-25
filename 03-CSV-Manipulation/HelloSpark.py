099ff8f59b5929e5c30fed95e0a0606dfc8251a0
karanbulani <ksbrocks.bulani@gmail.com>
2024-12-26 00:26:15 +0530

    CSV Manipulation with filter with repartition

--- 03-CSV-Manipulation/HelloSpark.py
+++ 03-CSV-Manipulation/HelloSpark.py
@@ -3,7 +3,7 @@ import sys
 from pyspark.sql import *
 
 from lib.logger import Log4j
-from lib.utils import get_spark_app_config, load_survey_df
+from lib.utils import get_spark_app_config, load_survey_df, count_by_country
 
 if __name__ == "__main__":
     conf = get_spark_app_config()
@@ -18,13 +18,12 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     logger.info("Starting HelloSpark")
+
     survey_df = load_survey_df(spark, sys.argv[1])
-    filtered_survey_df = survey_df.where("Age < 40") \
-        .select("Age", "Gender", "Country", "state")
-    grouped_df = filtered_survey_df.groupBy("Country")
-    count_df = grouped_df.count()
-    count_df.show()
+    partitioned_survey_df = survey_df.repartition(2)
+    count_df = count_by_country(partitioned_survey_df)
+    logger.info(count_df.collect())
 
-
+    input("Press Enter")
     logger.info("Finished HelloSpark")
-    spark.stop()
+    #spark.stop()
